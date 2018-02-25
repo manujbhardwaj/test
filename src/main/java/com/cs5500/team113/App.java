13cea5dccd16c5702ffa88d9da2fa217de562718
manujbhardwaj19 <manujbhardwaj19@ccs.neu.edu>
2018-02-24 19:28:50 -0500

    done

---
+++ src/main/java/com/cs5500/team113/App.java
@@ -0,0 +1,20 @@
+package com.cs5500.team113;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /**
+         * This is the main method. Requires no implementation
+         */
+
+        System.out.println(SayHello());
+    }
+    public static String SayHello(){
+        return "Hi2";
+    }
+}
---
+++ src/test/java/com/cs5500/team113/AppTest.java
@@ -0,0 +1,16 @@
+package com.cs5500.team113;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testPrintHelloWorld2() {
+
+        Assert.assertEquals(App.SayHello(), "Hi2");
+
+    }
+}
